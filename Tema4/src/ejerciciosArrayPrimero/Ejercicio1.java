0e137c3120d54ded762cd895ca47c9a8f9bbfe5b
jlmejorada <joseluis.mejorada@iesnervion.es>
2024-01-09 14:40:27 +0100

    Subida ej1,2

--- Tema4/src/ejerciciosArrayPrimero/Ejercicio1.java
+++ Tema4/src/ejerciciosArrayPrimero/Ejercicio1.java
@@ -1,9 +1,24 @@
 package ejerciciosArrayPrimero;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//Creamos un array de 10 de longitud
+		int tabla[] = new int [10];
+		//Inicializamos el número aleatorio
+		Random rn = new Random();
+		
+		//Un bucle que se repita el número de veces de longitud de la tabla
+		for (int i=0 ; i<tabla.length ; i++) {
+			//Va registrando números aleatorios entre 1 y 100
+			tabla[i] = rn.nextInt(1,101);
+		}
+		
+		// Imprimimos el array
+		System.out.println(Arrays.toString(tabla));
 
 	}
 
---
+++ Tema4/src/ejerciciosArrayPrimero/Ejercicio2.java
@@ -0,0 +1,29 @@
+package ejerciciosArrayPrimero;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		// Creamos un array de 5 de longitud
+		float tabla[] = new float[5];
+		// Inicializamos el número aleatorio
+		Random rn = new Random();
+		//Inicializamos el escaner
+		Scanner sc = new Scanner(System.in);
+
+		// Un bucle que se repita el número de veces de longitud de la tabla
+		for (int i = 0; i < tabla.length; i++) {
+			// Va registrando números aleatorios entre 1 y 100
+			System.out.println("Dame el número " + (i+1));
+			tabla[i] = sc.nextFloat();
+		}
+		// Imprimimos los números
+		for (float numero : tabla) {
+			System.out.println(numero);
+		}
+
+	}
+
+}
