04ad2b201d8b92d6baa2dcab0a76925f3872fb36
jlmejorada <joseluis.mejorada@iesnervion.es>
2024-01-10 08:47:03 +0100

    Subida ej4

---
+++ Tema4/src/ejerciciosArrayPrimero/Ejercicio4.java
@@ -0,0 +1,21 @@
+package ejerciciosArrayPrimero;
+
+import java.util.Arrays;
+
+public class Ejercicio4 {
+
+	public static void main(String[] args) {
+		// Creamos un array de 12 de longitud
+		int tabla[] = new int[12];
+		//Asignamos los valores
+		tabla[0]=39;
+		tabla[1]=-2;
+		tabla[4]=0;
+		tabla[6]=14;
+		tabla[8]=5;
+		tabla[9]=120;
+		//Muestra por pantalla el array
+		System.out.println(Arrays.toString(tabla));
+	}
+
+}
