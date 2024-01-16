acea3bcb7d79ec84052b0ebd24efbae0462d1d26
jlmejorada <joseluis.mejorada@iesnervion.es>
2024-01-16 14:05:16 +0100

    Subida ej5

---
+++ Tema4/src/claseArray/Ejercicio05.java
@@ -0,0 +1,28 @@
+package claseArray;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Ejercicio05 {
+
+	public static void main(String[] args) {
+		int tabla[]= new int[8];
+		
+		Scanner sc = new Scanner(System.in);
+		
+		for (int i = 0 ; i<tabla.length; i++) {
+			System.out.println("Dame la puntuación del jugador" + (i+1));
+			tabla[i]=sc.nextInt();
+		}
+		
+		Arrays.sort(tabla);
+		
+		for (int i = tabla.length-1 ; i>=0; i--) {
+			System.out.println(tabla[i]);
+		}
+
+	
+
+	}
+
+}
