02b4bbdd15e9f646111dc4a2fe45d8d25cb1e620
jlmejorada <joseluis.mejorada@iesnervion.es>
2024-01-18 12:58:38 +0100

    upd ej1

--- Tema4/src/funciones/Ejercicio01.java
+++ Tema4/src/funciones/Ejercicio01.java
@@ -8,13 +8,13 @@ public class Ejercicio01 {
 		// Creamos un array de numeros enteros
 		int tabla[] = { 1, 2, 3, 4, 5, 6 };
 		// Llamamos a la función que suma los número del array
-		suma = SumaArray(tabla);
+		suma = sumaArray(tabla);
 		// Mostramos por pantalla el resultado
 		System.out.println(suma);
 	}
 
 	// Recibe un array y suma los números enteros que lo componen
-	public static int SumaArray(int tabla[]) {
+	public static int sumaArray(int tabla[]) {
 		// Creamos una variable que recogera el resultado de la suma
 		int res = 0;
 		// Bucle que recorre el array
