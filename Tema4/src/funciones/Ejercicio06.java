97fa02d1722d6e132553b1eadcf70973c834949a
jlmejorada <joseluis.mejorada@iesnervion.es>
2024-01-26 14:23:42 +0100

    UPD ej6

--- Tema4/src/funciones/Ejercicio06.java
+++ Tema4/src/funciones/Ejercicio06.java
@@ -1,10 +1,25 @@
 package funciones;
 
+import java.util.Arrays;
+
 public class Ejercicio06 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int numero = 3;
+		int t[] = {1, 2, 3, 4, 5, 6}; 
 
+		System.out.println(Arrays.toString(suma(t,numero)));
+	}
+	
+	public static int[] suma(int t[], int numElementos) {
+		int sumaElementos[] = new int [t.length-numElementos+1];
+		for (int i = 0 ; i < sumaElementos.length ; i++) {
+			for (int j = i ; j<i+numElementos ; j++) {
+				sumaElementos[i] += t[j];
+			}
+		}
+		
+		return sumaElementos;
 	}
 
 }
