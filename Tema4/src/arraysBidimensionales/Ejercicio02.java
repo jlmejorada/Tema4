93599433d3360a99e92a9757c1ca4d2aa93e80eb
jlmejorada <joseluis.mejorada@iesnervion.es>
2024-01-30 14:21:22 +0100

    UPD ej2

--- Tema4/src/arraysBidimensionales/Ejercicio02.java
+++ Tema4/src/arraysBidimensionales/Ejercicio02.java
@@ -4,9 +4,12 @@ import java.util.Scanner;
 
 public class Ejercicio02 {
 
+	public static final int NUMALUM = 4;
+	public static final int NUMASIG = 5;
+
 	public static void main(String[] args) {
 		// Creamos un arraybidimensial 4 x 5
-		int[][] notas = new int[4][5];
+		int[][] notas = new int[NUMALUM][NUMASIG];
 		// Variables
 		float notaMedia;
 		int notaMax, notaMin;
@@ -33,7 +36,7 @@ public class Ejercicio02 {
 		for (int i=0 ; i<notas.length ; i++) {
 			notaMedia=0; 
 			notaMax=0;
-			notaMin=0;
+			notaMin=10;
 			System.out.print("Alumno " + (i+1) +" \t");
 			for (int j=0 ; j<notas[0].length ; j++ ) {
 				if (notaMin>notas[i][j]) {
@@ -44,7 +47,7 @@ public class Ejercicio02 {
 				}
 				notaMedia+=notas[i][j];
 			}
-			notaMedia=notaMedia/5;
+			notaMedia=notaMedia/NUMASIG;
 			System.out.println("nota media: " + notaMedia + " nota Maxima: " + notaMax + " nota Minima: " + notaMin);
 		}
 
