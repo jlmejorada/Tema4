91128f0ede4385f05d35d0d7cd2d134a04f4f47a
jlmejorada <joseluis.mejorada@iesnervion.es>
2024-01-31 10:24:45 +0100

    SUBIDA EJ6

---
+++ Tema4/src/arraysBidimensionales/Ejercicio06.java
@@ -0,0 +1,15 @@
+package arraysBidimensionales;
+
+public class Ejercicio06 {
+
+	public static void main(String[] args) {
+		int[][] tabla = new int [6][10]; 
+		
+
+	}
+	
+	public static void buscaExtremos (int[][] tabla) {
+		
+	}
+
+}
