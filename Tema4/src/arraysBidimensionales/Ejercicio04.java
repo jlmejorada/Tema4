dd85cc3875331851a0f50c62bb9b2202d5214c9c
mamej <mamej@MejoradaMarin>
2024-02-04 19:10:20 +0100

    UPD EJ4 COMENTARIOS

--- Tema4/src/arraysBidimensionales/Ejercicio04.java
+++ Tema4/src/arraysBidimensionales/Ejercicio04.java
@@ -3,14 +3,17 @@ package arraysBidimensionales;
 public class Ejercicio04 {
 
 	public static void main(String[] args) {
+		//Creamos una tabla 10x10
 		int[][] mul = new int[10][10];
-
+		//Recorremos las filas
 		for (int i=0 ; i<mul.length ; i++){
+			//Recorremos las columnas
 			for (int j=0 ; j<mul[0].length ; j++ ) {
+				//Registramos el numero de columna + 1 * numero de fila + 1
 				mul [i][j] = (j+1)*(i+1);
 			}
 		}
-		
+		//Imprimimos la tabla
 		for (int i=0 ; i<mul.length ; i++) {
 			for (int j=0 ; j<mul[0].length ; j++ ) {
 				System.out.print(mul[i][j] + "\t");
