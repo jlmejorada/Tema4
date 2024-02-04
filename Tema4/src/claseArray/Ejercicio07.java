00c8e266d6d209e76739d83b39058861cee1c2b3
mamej <mamej@MejoradaMarin>
2024-02-04 17:55:39 +0100

    UPD EJ7 COMENTARIOS

--- Tema4/src/claseArray/Ejercicio07.java
+++ Tema4/src/claseArray/Ejercicio07.java
@@ -6,30 +6,43 @@ import java.util.Random;
 public class Ejercicio07 {
 
 	public static void main(String[] args) {
+		// Creamos una tabla y registramos los numeros ganadores
 		int[] ganadora = { 3, 6, 9, 15, 32, 48 };
+		// y creamos otra con el mismo tamaño
 		int[] apuesta = new int[6];
+		//Creamos una variable que registrara el número con el que trabajaremos
 		int numero;
+		//otra para los aciertos
 		int aciertos = 0;
+		//y otra que será un contador
 		int i = 0;
+		//Inicializamos el numero random
 		Random rand = new Random();
-
+		//Recorremos la tabla apuesta
 		while (i < apuesta.length) {
+			//y le asignamos un numero aleatroio
 			numero = rand.nextInt(1, 50);
-
+			
+			//Miramos que el numero asigando no esté repetido
 			if (busqueda(apuesta, numero) < 0) {
 				apuesta[i] = numero;
+				//avanzamos el contador
 				i++;
 			}
 		}
 
+		//Imprimimos la apuesta generada
 		System.out.println(Arrays.toString(apuesta));
 
+		//La recorremos
 		for (int valor : apuesta) {
+			//Y vemos el numero de aciertos
 			if (Arrays.binarySearch(ganadora, valor) >= 0) {
 				aciertos++;
 			}
 		}
 
+		// Los mostramos por pantalla
 		System.out.println("Tienes " + aciertos + " aciertos.");
 
 	}
