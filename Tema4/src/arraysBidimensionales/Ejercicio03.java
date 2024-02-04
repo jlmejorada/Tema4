b349c77c043baf94bde32f6d63a2978ac4d22d02
mamej <mamej@MejoradaMarin>
2024-02-04 19:06:49 +0100

    UPD EJ3 COMENTARIOS

--- Tema4/src/arraysBidimensionales/Ejercicio03.java
+++ Tema4/src/arraysBidimensionales/Ejercicio03.java
@@ -1,13 +1,17 @@
 package arraysBidimensionales;
 
 public class Ejercicio03 {
-
+	//Constante para las filas
 	public static final int ROW = 4;
+	//Y las columnas
 	public static final int COL = 5;
-	
+	//Función Main
 	public static void main(String[] args) {
+		//Creamos una tabla bidimensional
 		int[][] tabla = new int [ROW][COL];
+		//Lanzamos la funcion cambiatabla
 		cambiaTabla(tabla);
+		//Imprimimos la tabla
 		for (int i=0 ; i<tabla.length ; i++) {
 			for (int j=0 ; j<tabla[0].length ; j++ ) {
 				System.out.print(tabla[i][j] + "\t");
