42cb8457fb870ba225450a88437e6216eb09c886
mamej <mamej@MejoradaMarin>
2024-02-04 16:54:49 +0100

    UPD EJ4 COMENTARIOS

--- Tema4/src/claseArray/Ejercicio04.java
+++ Tema4/src/claseArray/Ejercicio04.java
@@ -5,14 +5,16 @@ import java.util.Arrays;
 public class Ejercicio04 {
 
 	public static void main(String[] args) {
+		//Creamos una tabla de 30 posiciones
 		int tabla[]= new int[30];
-		
+		//La recorremos gracias a un bucle for
 		for (int i = 0 ; i<tabla.length; i++) {
+			//Le asignamos un numero aleatorio entre 0-9 a cada posicion
 			tabla[i]=(int)(Math.random()*10);
 		}
-		
+		//Usamos la funcion Arrays.sort, que ordena el array
 		Arrays.sort(tabla);
-		
+		//Lo mostramos por pantalla
 		System.out.println(Arrays.toString(tabla));
 
 	}
