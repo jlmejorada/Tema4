06515bd647e5d1f247057141c48298b15ebd72d7
jlmejorada <joseluis.mejorada@iesnervion.es>
2024-02-07 09:41:56 +0100

    SUBIDA EJ2

---
+++ Tema4/src/cadenas/Ejercicio02.java
@@ -0,0 +1,18 @@
+package cadenas;
+
+import java.util.Scanner;
+
+public class Ejercicio02 {
+
+	public static void main(String[] args) {
+		// Inicializamos el escaner
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		
+		//Cerramos el escaner
+		sc.close();
+	}
+
+}
