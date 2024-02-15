b38bdaf805c03750b917956aead2e48e010a8116
jlmejorada <joseluis.mejorada@iesnervion.es>
2024-02-15 13:57:58 +0100

    SUBIDA EJ9

---
+++ Tema4/src/cadenas/Ejercicio09.java
@@ -0,0 +1,22 @@
+package cadenas;
+
+public class Ejercicio09 {
+
+	public static void main(String[] args) {
+		// Strings para la frase introducida
+		// Javalín, javalón             javalén, len, len
+		String frase="Javalín, javalón \t";
+		//Llamamos a la función e imprimimos lo que nos devuelve
+		System.out.println(traductor(frase));
+
+	}
+
+	public static String traductor (String frase) {
+		// Creamos una variable 
+		String traduccion = "";
+		
+		
+		//devolvemos la traduccion
+		return traduccion;
+	}
+}
