caef405d06c9739af6ba35e9ee367157a53b7467
mamej <mamej@MejoradaMarin>
2024-02-15 19:01:52 +0100

    UPD EJ9

--- Tema4/src/cadenas/Ejercicio09.java
+++ Tema4/src/cadenas/Ejercicio09.java
@@ -3,20 +3,33 @@ package cadenas;
 public class Ejercicio09 {
 
 	public static void main(String[] args) {
-		// Strings para la frase introducida
-		// Javalín, javalón             javalén, len, len
-		String frase="Javalín, javalón \t";
-		//Llamamos a la función e imprimimos lo que nos devuelve
-		System.out.println(traductor(frase));
+	    // Strings para la frase introducida
+	    // Javalín, javalón             javalén, len, len
+	    String frase = "Javalín, javalón \t me llamo Pablo Carbonero \t javalén, len, len";
+	    // Llamamos a la función e imprimimos lo que nos devuelve
+	    System.out.println(traductor(frase));
 
 	}
 
-	public static String traductor (String frase) {
-		// Creamos una variable 
-		String traduccion = "";
-		
-		
-		//devolvemos la traduccion
-		return traduccion;
+	public static String traductor(String frase) {
+	    // Creamos una constante para el "prefijo" jabalin jabalon
+	    final String PREFIJO = "Javalín, javalón";
+	    // Creamos una constante para el "sufijo" jabalin jabalon
+	    final String SUFIJO = "javalén, len, len";
+	    // Creamos una variable para la traducción final
+	    String traduccion = "";
+	    // Un array para guardar las frases divididas por los tab usados
+	    String[] fraseDividida;
+	    // Guardamos la frase dividida en array
+	    fraseDividida = frase.split("\t");
+	    // Recorremos el array
+	    for (int i = 0; i < fraseDividida.length; i++) {
+	        // Verificamos si la parte actual contiene el prefijo o sufijo
+	        if (!fraseDividida[i].contains(PREFIJO) && !fraseDividida[i].contains(SUFIJO)) {
+	            traduccion += fraseDividida[i];
+	        }
+	    }
+	    // devolvemos la traduccion
+	    return traduccion;
 	}
 }
