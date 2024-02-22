d7d50d6a90d1224bbfa1ffdb7b983d1c99e56e1f
mamej <mamej@MejoradaMarin>
2024-02-22 19:30:08 +0100

    COMENTARIOS EJ13

--- Tema4/src/cadenas/Ejercicio13.java
+++ Tema4/src/cadenas/Ejercicio13.java
@@ -6,48 +6,49 @@ import java.util.Scanner;
 public class Ejercicio13 {
 
 	public static void main(String[] args) {
+		//Inicializamos el escaner
 		Scanner sc = new Scanner(System.in);
-
-		String palabra = "";
-
-		String anagrama = "";
-
-		boolean resultado;
-
+		//Variable que guardara la primera palabra
+		String palabra1 = "";
+		//Variable que guardara la segunda palabra
+		String palabra2 = "";
+		//Variable que recogera el resultado si es o no anagrama
+		boolean flag = false;
+		//Pedimos la primera palabra y la registramos
 		System.out.println("Introduce una palabra");
-		palabra = sc.next().toLowerCase();
+		palabra1 = sc.next().toLowerCase();
+		//Pedimos la segunda palabra y la registramos
+		System.out.println("Introduce un anagrama de la palabra anterior");
+		palabra2 = sc.next().toLowerCase();
+		//Llamamos a la función y recogemos a traves de una boleean si es o no anagrama
+		flag = esAnagrama(palabra1, palabra2);
+		//Si la bandera marca true
+		if(flag) {
+			//Es un anagrama
+			System.out.println("Ambas palabras actuan como anagrama de la otra");
+			//Si no, no son anagramas
+		}else System.out.println("Estas palabras no son anagramas");
 
-		System.out.println("Introduce el anagrama de la palabra anterior");
-		anagrama = sc.next().toLowerCase();
-
-		resultado = esAnagrama(palabra, anagrama);
-
-		if(resultado) {
-			System.out.println("Si es un anagrama");
-		}else {
-			System.out.println("No es un anagrama");
-		}
-		
+		//Cerramos el escaner
 		sc.close();
 	}
 
 	public static boolean esAnagrama(String palabra, String anagrama) {
-
-		boolean esAn = true;
-
+		//Marca si la condición es correcta
+		boolean esAnagrama = false;
+		//Metemmos ambas palabras en unos char array
 		char[] letraP = palabra.toCharArray();
-
 		char[] letraA = palabra.toCharArray();
-
+		// Y los ordenamos
 		Arrays.sort(letraA);
-
 		Arrays.sort(letraP);
-
-		if (!Arrays.equals(letraP, letraA)) {
-			esAn = false;
+		//Si son iguales
+		if (Arrays.equals(letraP, letraA)) {
+			//Marcamos la boleean como true
+			esAnagrama = true;
 		}
-
-		return esAn;
+		//Devolvemos la boleean
+		return esAnagrama;
 
 	}
 
